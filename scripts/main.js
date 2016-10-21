2549e701ef4e7070f2b48110332e8dd8603248c3
Robert Buckley <cardinalsmurf@gmail.com>
2016-10-21 16:09:55 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World!';
+
+var myImg = document.querySelector('img');
+myImg.onclick = function() {
+  var mySrc = myImg.getAttribute('src');
+  if (mySrc === 'images/firefox-icon.png') {
+    myImg.setAttribute('src', 'images/lena256x256.something');
+  } else {
+    myImg.setAttribute('src', 'images/firefox-icon.png');
+  }
+}
